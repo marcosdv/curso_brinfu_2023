da4ef23c6def999a7a3d1109e8ec5b42786f9dfc
Marcos Dias Vendramini <marcosdiasvendramini@yahoo.com.br>
2023-08-24 20:43:33 -0300

    Add files via upload

---
+++ NoticiasApi/NoticiasApi/Controllers/NoticiasController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NoticiasApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class NoticiasController : ControllerBase
+{
+    [HttpGet("Ola")]
+    public string BuscarTodas()
+    {
+        return "Relou Uordi!!!";
+    }
+}
